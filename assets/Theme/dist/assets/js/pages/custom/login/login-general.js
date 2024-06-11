5159f52857000115fd4bd0c336876525c932c8d1
Edwin Perez <lic.edwin.perez@gmail.com>
2024-06-11 11:50:25 -0600

    cambio en el js del login

--- assets/Theme/dist/assets/js/pages/custom/login/login-general.js
+++ assets/Theme/dist/assets/js/pages/custom/login/login-general.js
@@ -24,25 +24,25 @@ var KTLogin = function() {
         validation = FormValidation.formValidation(
             KTUtil.getById('kt_login_signin_form'), {
                 fields: {
-                    identity: {
+                    username: {
                         validators: {
                             notEmpty: {
-                                message: 'El campo email/usuario es requerido'
+                                message: 'El usuario es requerido'
                             }
                         }
                     },
                     password: {
                         validators: {
                             notEmpty: {
-                                message: 'El campo contraseña es requerido'
+                                message: 'La cotraseña es requerida'
                             }
                         }
                     }
                 },
                 plugins: {
                     trigger: new FormValidation.plugins.Trigger(),
-                    // submitButton: new FormValidation.plugins.SubmitButton(),
-                    defaultSubmit: new FormValidation.plugins.DefaultSubmit(), // Uncomment this line to enable normal button submit after form validation
+                    submitButton: new FormValidation.plugins.SubmitButton(),
+                    //defaultSubmit: new FormValidation.plugins.DefaultSubmit(), // Uncomment this line to enable normal button submit after form validation
                     bootstrap: new FormValidation.plugins.Bootstrap()
                 }
             }
@@ -52,22 +52,72 @@ var KTLogin = function() {
             e.preventDefault();
 
             validation.validate().then(function(status) {
-                // console.log(status);
                 if (status == 'Valid') {
-                    swal.fire({
-                        text: "¡Todo es genial! Ahora envía este formulario",
-                        icon: "success",
-                        buttonsStyling: false,
-                        confirmButtonText: "Ok, vamos!",
-                        customClass: {
-                            confirmButton: "btn font-weight-bold btn-light-primary"
+
+                    $.ajax({
+                        url: PATH + 'auth/login',
+                        method: "POST",
+                        dataType: "json",
+                        data: $('#kt_login_signin_form').serialize(),
+                        beforeSend: function() {
+
+                        },
+                        success: function(result) {
+                            // debugger;
+                            // console.log(result);
+
+                            if (result.estatus == 'error') {
+                                swal.fire({
+                                    text: result.mensaje,
+                                    icon: "error",
+                                    buttonsStyling: false,
+                                    confirmButtonText: "Ok, vamos!",
+                                    customClass: {
+                                        confirmButton: "btn font-weight-bold btn-light-primary"
+                                    }
+                                }).then(function() {
+                                    KTUtil.scrollTop();
+                                });
+                            } else {
+                                console.log(result.estatus);
+
+                                swal.fire({
+                                    text: result.mensaje,
+                                    icon: "success",
+                                    buttonsStyling: false,
+                                    confirmButtonText: "Ok, vamos!",
+                                    customClass: {
+                                        confirmButton: "btn font-weight-bold btn-light-primary"
+                                    }
+                                }).then(function() {
+                                    window.location.href = PATH + 'auth/index';
+                                });
+
+                            }
+
+                        },
+                        complete: function() {
+
+                        },
+                        error: function() {
+                            swal.fire({
+                                text: "Disculpa, parece que se detectaron algunos errores, por favor intentalo nuevamente.",
+                                icon: "error",
+                                buttonsStyling: false,
+                                confirmButtonText: "Ok, vamos!",
+                                customClass: {
+                                    confirmButton: "btn font-weight-bold btn-light-primary"
+                                }
+                            }).then(function() {
+                                KTUtil.scrollTop();
+                            });
                         }
-                    }).then(function() {
-                        KTUtil.scrollTop();
                     });
+
+
                 } else {
                     swal.fire({
-                        text: "Disculpa, parece que se han detectado algunos errores, inténtalo de nuevo.",
+                        text: "Disculpa, parece que se detectaron algunos errores, por favor intentalo nuevamente.",
                         icon: "error",
                         buttonsStyling: false,
                         confirmButtonText: "Ok, vamos!",
@@ -102,50 +152,102 @@ var KTLogin = function() {
         validation = FormValidation.formValidation(
             form, {
                 fields: {
-                    fullname: {
+                    first_name: {
                         validators: {
                             notEmpty: {
-                                message: 'Username is required'
+                                message: 'Nombre es requerido'
                             }
                         }
                     },
-                    email: {
+                    ap1: {
                         validators: {
                             notEmpty: {
-                                message: 'Email address is required'
+                                message: 'Primer apellido es requerido'
+                            }
+                        }
+                    },
+                    company: {
+                        validators: {
+                            notEmpty: {
+                                message: 'El departamento es requerido'
+                            }
+                        }
+                    },
+                    phone: {
+                        validators: {
+                            notEmpty: {
+                                message: 'El teléfono es requerido'
                             },
-                            emailAddress: {
-                                message: 'The value is not a valid email address'
+                            stringLength: {
+                                max: 10,
+                                message: 'El teléfono debe tener por minimo 10 digitos',
+                            },
+                            integer: {
+                                message: 'El valor no es un numero',
+                                // The default separators
+                                thousandsSeparator: '',
+                                decimalSeparator: '',
+                            },
+                            phone: {
+                                message: 'El formato no es valido, favor de verificar'
                             }
                         }
                     },
+                    // email: {
+                    //     validators: {
+                    //         notEmpty: {
+                    //             message: 'Correo electronico es requerido'
+                    //         },
+                    //         emailAddress: {
+                    //             message: 'El formato de correo electrónico no es valido, favor de verificar'
+                    //         }
+                    //     }
+                    // },
+                    // identity: {
+                    //     validators: {
+                    //         notEmpty: {
+                    //             message: 'El usuario es requerido'
+                    //         }
+                    //     }
+                    // },
                     password: {
                         validators: {
+                            stringLength: {
+                                max: 4,
+                                min: 4,
+                                message: 'El teléfono debe tener 4 digitos',
+                            },
+                            integer: {
+                                message: 'El valor no es un numero',
+                                // The default separators
+                                thousandsSeparator: '',
+                                decimalSeparator: '',
+                            },
                             notEmpty: {
-                                message: 'The password is required'
+                                message: 'El NIP es requerido'
                             }
                         }
                     },
-                    cpassword: {
+                    password_confirm: {
                         validators: {
                             notEmpty: {
-                                message: 'The password confirmation is required'
+                                message: 'La confirmación del NIP es requerida'
                             },
                             identical: {
                                 compare: function() {
                                     return form.querySelector('[name="password"]').value;
                                 },
-                                message: 'The password and its confirm are not the same'
+                                message: 'La confirmación del NIP no coincide, favor de verificar'
                             }
                         }
                     },
-                    agree: {
-                        validators: {
-                            notEmpty: {
-                                message: 'You must accept the terms and conditions'
-                            }
-                        }
-                    },
+                    // agree: {
+                    //     validators: {
+                    //         notEmpty: {
+                    //             message: 'Debes de aceptar los terminos y condiciones'
+                    //         }
+                    //     }
+                    // },
                 },
                 plugins: {
                     trigger: new FormValidation.plugins.Trigger(),
@@ -159,23 +261,78 @@ var KTLogin = function() {
 
             validation.validate().then(function(status) {
                 if (status == 'Valid') {
-                    swal.fire({
-                        text: "All is cool! Now you submit this form",
-                        icon: "success",
-                        buttonsStyling: false,
-                        confirmButtonText: "Ok, got it!",
-                        customClass: {
-                            confirmButton: "btn font-weight-bold btn-light-primary"
+
+                    // console.log(form);
+
+
+
+                    $.ajax({
+                        url: PATH + 'auth/create_user',
+                        method: "POST",
+                        dataType: "json",
+                        data: $('#kt_login_signup_form').serialize(),
+                        beforeSend: function() {
+
+                        },
+                        success: function(result) {
+                            // debugger;
+                            console.log(result);
+
+                            if (result.estatus == 'error') {
+                                swal.fire({
+                                    text: "Disculpa, parece que se detectaron algunos errores, por favor intentalo nuevamente.",
+                                    icon: "error",
+                                    buttonsStyling: false,
+                                    confirmButtonText: "Ok, vamos!",
+                                    customClass: {
+                                        confirmButton: "btn font-weight-bold btn-light-primary"
+                                    }
+                                }).then(function() {
+                                    KTUtil.scrollTop();
+                                });
+                            } else {
+                                swal.fire({
+                                    text: "Todo salió correctamente! Se enviarán los datos",
+                                    icon: "success",
+                                    buttonsStyling: false,
+                                    confirmButtonText: "Ok, vamos!",
+                                    customClass: {
+                                        confirmButton: "btn font-weight-bold btn-light-primary"
+                                    }
+                                }).then(function() {
+                                    _showForm('signin');
+                                });
+
+                            }
+
+                        },
+                        complete: function() {
+
+                        },
+                        error: function() {
+                            swal.fire({
+                                text: "Disculpa, parece que se detectaron algunos errores, por favor intentalo nuevamente.",
+                                icon: "error",
+                                buttonsStyling: false,
+                                confirmButtonText: "Ok, vamos!",
+                                customClass: {
+                                    confirmButton: "btn font-weight-bold btn-light-primary"
+                                }
+                            }).then(function() {
+                                KTUtil.scrollTop();
+                            });
                         }
-                    }).then(function() {
-                        KTUtil.scrollTop();
                     });
+
+
+
+
                 } else {
                     swal.fire({
-                        text: "Sorry, looks like there are some errors detected, please try again.",
+                        text: "Disculpa, parece que se detectaron algunos errores, por favor intentalo nuevamente.",
                         icon: "error",
                         buttonsStyling: false,
-                        confirmButtonText: "Ok, got it!",
+                        confirmButtonText: "Ok, vamos!",
                         customClass: {
                             confirmButton: "btn font-weight-bold btn-light-primary"
                         }
